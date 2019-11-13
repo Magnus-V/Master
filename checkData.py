2822776e0d40cfac4fc8a69956aa4ec7aab7fa4a
Magnus-V <magnves@gmail.com>
2019-11-13 22:55:42 +0100

    Noe protokoder

--- checkData.py
+++ checkData.py
@@ -30,7 +30,7 @@ def readCSVSurveyConvertToDataFrame(csvfile):
 df2017 = readCSVSurveyConvertToDataFrame(livingConditionsSurvey2017)
 
 
-print(list(df2017.columns))
+#print(list(df2017.columns))
 #avgInntekt = df2['saminnt_su'].mean()
 #avgLonnSu = df2['lonn_su'].mean()
 #avgLonnEtterSkatt = df2017['wies_3'].mean()
@@ -40,4 +40,31 @@ def findCorrelation(x, y):
     corrArray = np.corrcoef(x, y)
     print(corrArray)
     return corrArray
+
+def functionThatFindsCorrelationBetweenHigherThanAveragePay():
+    # ha et array med gjennomsnittsverdier som gjenspeiler de verdier som gjenspeiler et gjennomsnittsperson i Norge i hver aldersgruppe
+    # Bruker skriver inn sin inntekt og alder
+
+    """"
+    OKAY.
+
+    Funksjon med array for gjennomsnittet, de under gj.snitt og de over gj.snitt. Finn de variablene som avviker mest.
+
+    """
+
+def createArrayWithAverageValues(dataFrame):
+    addition = 0
+    noRow = 0
+    for row in dataFrame:
+        addition =+ dataFrame['wies_3'].values(row)
+        noRow =+ 1
+    return addition/noRow
+
+createArrayWithAverageValues(df2017)
+
+def createArrayForThoseWithHigherPayAndTheirAverageValues():
+    return
+
+def createArrayForThoseWithLowerPayAndTheirAverageValues():
+    return
 
